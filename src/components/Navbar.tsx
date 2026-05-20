371b829e0d9c91701c9c8fed518f50fb5260911a
Tunde Abiona <tunde.abiona@gmail.com>
2026-05-20 06:20:55 +0100

    fixed responsive mobile navbar

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,23 +1,65 @@
+import { useState } from "react";
 import { Link } from "react-router-dom";
 import logo from "../assets/sampro-logo.png";
 
 export default function Navbar() {
+
+	const [menuOpen, setMenuOpen] = useState(false);
+
 	return (
 		<nav className="navbar">
 			<div className="container nav-wrapper">
+
+				{/* LOGO */}
 				<div className="logo-box">
-					<img src={logo} className="logo" alt="SamPro Logo" />
+					<img
+						src={logo}
+						className="logo"
+						alt="SamPro Logo"
+					/>
 				</div>
 
-				<div className="menu">
-					<Link to="/">Home</Link>
-					<Link to="/about">About Us</Link>
-					<Link to="/team">Our Team</Link>
-					<Link to="/companies">Companies</Link>
-					<Link to="/why-sampro">Why SamPro</Link>
-					{/* <Link to="/applications">Applications</Link> */}
-					<Link to="/contact">Contact</Link>
+				{/* MOBILE MENU BUTTON */}
+				<div
+					className={`menu-toggle ${menuOpen ? "active" : ""}`}
+					onClick={() => setMenuOpen(!menuOpen)}
+				>
+					<span></span>
+					<span></span>
+					<span></span>
 				</div>
+
+				{/* MENU */}
+				<div className={`menu ${menuOpen ? "active" : ""}`}>
+
+					<Link to="/" onClick={() => setMenuOpen(false)}>
+						Home
+					</Link>
+
+					<Link to="/about" onClick={() => setMenuOpen(false)}>
+						About Us
+					</Link>
+
+					<Link to="/team" onClick={() => setMenuOpen(false)}>
+						Our Team
+					</Link>
+
+					<Link to="/companies" onClick={() => setMenuOpen(false)}>
+						Companies
+					</Link>
+
+					<Link to="/why-sampro" onClick={() => setMenuOpen(false)}>
+						Why SamPro
+					</Link>
+
+					{/* <Link to="/applications">Applications</Link> */}
+
+					<Link to="/contact" onClick={() => setMenuOpen(false)}>
+						Contact
+					</Link>
+
+				</div>
+
 			</div>
 		</nav>
 	);
